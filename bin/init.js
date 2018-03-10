5227381683ec6ce1fd2d0e06d6f6d64ab0ef9b66
Ben Priddy <ben.priddy@toolofna.com>
2018-03-10 14:56:20 -0500

    add spawn process to install npm

--- bin/init.js
+++ bin/init.js
@@ -2,9 +2,9 @@ const path = require('path')
 const ncp = require('ncp')
 const fs = require('fs');
 const chalk = require('chalk');
+const { spawn } = require('child_process');
 
 module.exports = () => {
-	console.log(__dirname)
 	console.log('init')
 	const source = path.resolve(__dirname, "../templates/functions/");
 	const destination = path.resolve(process.cwd(),`functions/`);
@@ -17,5 +17,18 @@ module.exports = () => {
 			return console.error(err);
 		}
 		console.log(chalk.green('Project successfully scaffolded!'));
+		console.log(chalk.yellow('Now installing dependencies...\n\n\n'));
+		const install =  spawn('npm', ['--prefix', './functions', 'install', './functions'])
+		install.stdout.on('data', (data) => {
+		  console.log(`stdout: ${data}`);
+		});
+
+		install.stderr.on('data', (data) => {
+		  console.log(`stderr: ${data}`);
+		});
+
+		install.on('close', (code) => {
+		  console.log(`child process exited with code ${code}`);
+		});
 	});
 }
